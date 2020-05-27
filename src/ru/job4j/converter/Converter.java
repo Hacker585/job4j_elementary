d8446f2f05dc5f0e769b478e38798f3b3b36ab24
Hacker585 <stasbogatin@mail.ru>
2020-05-27 16:02:31 +0300

    Сделал программу для нахождения расстояния между двумя точками

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,16 +1,16 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static int rubleToEuro(int value) {
-      int rsl = value / 70;
-      return rsl;
+    public static int rubleToEuro(int value) {// перед именем метода стоит тип переменной который вернёт этот метод (int)
+      int result = value / 70;
+      return result;
     }
     public static int rubleToDollar(int value) {
         int rsl = value / 60;
         return rsl;
     }
 
-    public static void main(String[]args){
+    public static void main(String[]args){// перед именем метода стоит void, значит он ничего не возвращает
         int euro = Converter.rubleToEuro(140);
         System.out.println(" 140 rubles are " + euro +" euro.");
         int dollar = Converter.rubleToDollar(120);
